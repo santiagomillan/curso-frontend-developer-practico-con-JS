b2335da833665e32c690bd95d621bd9875803399
Santiago Millan <104397283+SantiagoNivelics@users.noreply.github.com>
2023-06-04 23:00:46 -0500

    clase 21

--- main.js
+++ main.js
@@ -2,17 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobilepMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
-    console.log('click')
+    console.log('click 5')
+    const isAsideClose = aside.classList.contains('inactive')
+
+    if(!isAsideClose){
+        aside.classList.add('inactive')
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilepMenu() {
-    console.log('click')
+    console.log('click 4')
+    const isAsideClose = aside.classList.contains('inactive')
+
+    if(!isAsideClose){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    console.log('click 3')
+    const isMobileClosed = mobileMenu.classList.contains('inactive')
+    const ismenuDesktopClosed = menuEmail.classList.contains('inactive')
+
+    if(!isMobileClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    if(!ismenuDesktopClosed){
+        desktopMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive');
 }
